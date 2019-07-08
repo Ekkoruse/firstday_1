09c0897b65a802492404cf2dd2a420f5e26c1208
Ekkoruse <Ekkoruse@outlook.com>
2019-07-09 02:14:49 +0800

    add some views

--- app/src/main/java/com/example/firstday_2/MainActivity.java
+++ app/src/main/java/com/example/firstday_2/MainActivity.java
@@ -11,6 +11,14 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,8 +37,98 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
 
+        Button btn1 = findViewById(R.id.button);
+        Button btn2 = findViewById(R.id.button2);
+        final Switch sw1=findViewById(R.id.switch1);
+        final TextView tv1=findViewById(R.id.textView);
+        final ImageView iv1=findViewById(R.id.imageView);
+        final CheckBox cb1=findViewById(R.id.checkBox);
+        final RadioGroup rg1=findViewById(R.id.radioGroup2);
+        final TextView tv2=findViewById(R.id.textView2);
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                //tv1.setText("aasd");
+                iv1.setVisibility(View.INVISIBLE);
+                sw1.setVisibility(View.INVISIBLE);
+                cb1.setVisibility(View.INVISIBLE);
+                tv1.setVisibility(View.INVISIBLE);
+                rg1.setVisibility(View.INVISIBLE);
+                tv2.setVisibility(View.INVISIBLE);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+               // tv1.setText("aasd");
+                iv1.setVisibility(View.VISIBLE);
+                sw1.setVisibility(View.VISIBLE);
+                cb1.setVisibility(View.VISIBLE);
+                tv1.setVisibility(View.VISIBLE);
+                rg1.setVisibility(View.VISIBLE);
+                tv2.setVisibility(View.VISIBLE);
+            }
+        });
+        Switch sw2=findViewById(R.id.switch1);
+        sw2.setOnClickListener(new View.OnClickListener(){
+            int i=1;
+            @Override
+            public void onClick(View v)
+            {
+                if(i==0)
+                {
+                    i=1;
+                    tv1.setText("Hello world!");
+                }
+                else
+                {
+                    tv1.setText("你好世界！");
+                    i=0;
+                }
+            }
+        });
+        CheckBox cb2=findViewById(R.id.checkBox);
+        cb2.setOnClickListener(new View.OnClickListener(){
+            int i=1;
+            @Override
+            public void onClick(View v)
+            {
+                if(i==0)
+                {
+                    i=1;
+                    tv1.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    tv1.setVisibility(View.INVISIBLE);
+                    i=0;
+                }
+            }
+        });
+        RadioGroup rag1=findViewById(R.id.radioGroup2);
+        rag1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group,int checkedId)
+            {
+                if(checkedId==2131230847)
+                {
+                    tv2.setText("choose 1");
+                }
+                else if(checkedId==2131230848)
+                {
+                    tv2.setText("choose 2");
+                }
+                else
+                {
+                    tv2.setText("choose 3");
+                }
+
+            }
+        });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
