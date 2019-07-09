0ee9b5ade571c951f99bb4352354bea84b5e8afc
Ekkoruse <Ekkoruse@outlook.com>
2019-07-09 14:48:58 +0800

    apk

--- app/src/main/java/com/example/firstday_2/MainActivity.java
+++ app/src/main/java/com/example/firstday_2/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.firstday_2;
+import android.content.Intent;
 
 import android.os.Bundle;
 
@@ -38,95 +39,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btn1 = findViewById(R.id.button);
-        Button btn2 = findViewById(R.id.button2);
-        final Switch sw1=findViewById(R.id.switch1);
-        final TextView tv1=findViewById(R.id.textView);
-        final ImageView iv1=findViewById(R.id.imageView);
-        final CheckBox cb1=findViewById(R.id.checkBox);
-        final RadioGroup rg1=findViewById(R.id.radioGroup2);
-        final TextView tv2=findViewById(R.id.textView2);
+        Button btn1;
+        btn1=findViewById(R.id.button2);
         btn1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v)
             {
-                //tv1.setText("aasd");
-                iv1.setVisibility(View.INVISIBLE);
-                sw1.setVisibility(View.INVISIBLE);
-                cb1.setVisibility(View.INVISIBLE);
-                tv1.setVisibility(View.INVISIBLE);
-                rg1.setVisibility(View.INVISIBLE);
-                tv2.setVisibility(View.INVISIBLE);
-            }
-        });
-        btn2.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v)
-            {
-                // tv1.setText("aasd");
-                iv1.setVisibility(View.VISIBLE);
-                sw1.setVisibility(View.VISIBLE);
-                cb1.setVisibility(View.VISIBLE);
-                tv1.setVisibility(View.VISIBLE);
-                rg1.setVisibility(View.VISIBLE);
-                tv2.setVisibility(View.VISIBLE);
-            }
-        });
-        Switch sw2=findViewById(R.id.switch1);
-        sw2.setOnClickListener(new View.OnClickListener(){
-            int i=1;
-            @Override
-            public void onClick(View v)
-            {
-                if(i==0)
-                {
-                    i=1;
-                    tv1.setText("Hello world!");
-                }
-                else
-                {
-                    tv1.setText("你好世界！");
-                    i=0;
-                }
-            }
-        });
-        CheckBox cb2=findViewById(R.id.checkBox);
-        cb2.setOnClickListener(new View.OnClickListener(){
-            int i=1;
-            @Override
-            public void onClick(View v)
-            {
-                if(i==0)
-                {
-                    i=1;
-                    tv1.setVisibility(View.VISIBLE);
-                }
-                else
-                {
-                    tv1.setVisibility(View.INVISIBLE);
-                    i=0;
-                }
-            }
-        });
-        RadioGroup rag1=findViewById(R.id.radioGroup2);
-        rag1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
-            @Override
-            public void onCheckedChanged(RadioGroup group,int checkedId)
-            {
-                if(checkedId==2131230847)       //this is one id of 3 radiobutton
-                //i dont know how to identify this
-                {
-                    tv2.setText("choose 1");
-                }
-                else if(checkedId==2131230848)
-                {
-                    tv2.setText("choose 2");
-                }
-                else
-                {
-                    tv2.setText("choose 3");
-                }
-
+                Intent intent = new Intent(MainActivity.this, DemoActivity.class);
+                startActivity(intent);
             }
         });
     }
